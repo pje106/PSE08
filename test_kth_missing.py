040d3e9de60c47b485ba05ee6e64f6cdb9ab8645
JuliaPark <pje106@gmail.com>
2022-12-04 12:49:18 -0800

    edit test file

--- test_kth_missing.py
+++ test_kth_missing.py
@@ -1,7 +1,7 @@
 import pytest
 from kth_Missing import kth_missing_positive_number
 
-def test_kth_missing_positive_number(numbers, k):
+def test_kth_missing_positive_number():
     
     #arrange #act
     result = kth_missing_positive_number([2,3,4,7,11],5)
